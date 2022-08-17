fcf80ba3c68aeb1ce17d2bda4c7abadb15f40bc3
BuildTools <unconfigured@null.spigotmc.org>
2022-08-17 21:33:43 +0200

    optimized commands handling

--- src/main/java/me/herrphoenix/obamabot/listener/ObamaListener.java
+++ src/main/java/me/herrphoenix/obamabot/listener/ObamaListener.java
@@ -35,27 +35,54 @@ public class ObamaListener extends MCBOTSessionAdapter {
         if (packet instanceof ClientboundChatPacket) {
             ClientboundChatPacket chatPacket = (ClientboundChatPacket) packet;
             String translatedMessage = TranslateChat.translateComponent(chatPacket.getMessage());
+
             if (translatedMessage.isEmpty())
                 translatedMessage = "Failed to translate message: " + chatPacket.getMessage();
 
-            if (translatedMessage.contains(".exempt")) {
-                String ign = translatedMessage.split(" ")[translatedMessage.split(" ").length -1];
+            //actual message sent by the player
+            String playerMessage = translatedMessage.split(":", 2)[1];
 
-                ObamaRegistry.getRegistry().addLifetime(ign);
+            //commands
+            if (playerMessage.startsWith("obama")) {
+                String[] args = playerMessage.split(" ", 2)[1].split(" ");
 
-                bot.getClient().send(new ServerboundChatPacket(ign + " will not be taxed"));
+                if (args.length >= 2) {
+                    if (args[0].equals("exempt")) {
+                        String ign = args[1];
+
+                        ObamaRegistry.getRegistry().addLifetime(ign);
+
+                        bot.getClient().send(new ServerboundChatPacket(ign + " will not be taxed"));
+                    }
+                    if (args[0].equals("triggerEntry")) {
+                        String ign = args[1];
+
+                        try {
+                            bot.getClient().send(new ServerboundChatPacket("Triggering plot entry for " + ign + " in 5 seconds."));
+                            Thread.sleep(5000);
+                            bot.getObama().handlePlayer(ign);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+
+                    }
+                    if (args[0].equals("check")) {
+                        String ign = args[1];
+
+                        if (ObamaRegistry.getRegistry().hasLifetime(ign)) {
+                            bot.getClient().send(new ServerboundChatPacket(ign + " has lifetime"));
+                            return;
+                        }
+                        if (ObamaRegistry.getRegistry().hasHourly(ign)) {
+                            bot.getClient().send(new ServerboundChatPacket(ign + " has paid a hourly tax and it will expire on " + ObamaRegistry.getRegistry().getExpire(translatedMessage.split(" ")[6])));
+                            return;
+                        }
+                        bot.getClient().send(new ServerboundChatPacket(ign + " has not paid anything."));
+                    }
+                }
             }
-            if (translatedMessage.contains(".check")) {
-                String ign = translatedMessage.split(" ")[translatedMessage.split(" ").length -1];
 
-                boolean lifetime = ObamaRegistry.getRegistry().hasLifetime(ign);
-                boolean hourly = ObamaRegistry.getRegistry().hasHourly(ign);
-
-                if (lifetime) { bot.getClient().send(new ServerboundChatPacket(ign + " has lifetime")); return; }
-                if (hourly) { bot.getClient().send(new ServerboundChatPacket(ign + " has paid a hourly tax and it will expire on " + ObamaRegistry.getRegistry().getExpire(translatedMessage.split(" ")[6]))); return; }
-                bot.getClient().send(new ServerboundChatPacket(ign + " has not paid anything."));
-            }
-
+            //someone entered the plot
             if (translatedMessage.endsWith(ObamaBOT.PLOT_ENTER)) {
                 String ign = translatedMessage.split(" ")[2];
                 if (ign == null) ign = translatedMessage.split(" ")[4];
